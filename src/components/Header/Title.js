29be4faa6fd0deaf5871d72d91a6c78b14e45f2c
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-04-26 20:17:17 +0200

    fixed recreate doc in firestore

--- src/components/Header/Title.js
+++ src/components/Header/Title.js
@@ -15,23 +15,33 @@ import {
   where,
 } from 'firebase/firestore';
 
-const Title = (props) => {
+const Title = ({ isLoggedIn, setIsLoggedIn }) => {
   const favContext = useContext(FavoritesContext);
   const [userName, setUserName] = useState(false);
-  const [favoritesListExist, setFavoritesListExist] = useState(false);
-
   const favoritesCollectionRef = collection(db, 'favorites');
 
+  const createFavoritesDoc = async () => {
+    await addDoc(favoritesCollectionRef, {
+      user: auth.currentUser.displayName,
+      id: auth.currentUser.uid,
+      favorites: favContext.favoriteItems,
+    });
+  };
+
   const getFavorites = async () => {
-    const data = await getDocs(favoritesCollectionRef);
+    const userRef = query(
+      collection(db, 'favorites'),
+      where('id', '==', auth.currentUser.uid)
+    );
+    const data = await getDocs(userRef);
     const favorites = data.docs.map((doc) => doc.data().favorites);
+    setIsLoggedIn(true);
     if (favorites[0] !== undefined) {
       favContext.setFavoriteItems([...favorites[0]]);
-      setFavoritesListExist(true);
-      props.setIsLoggedIn(true);
+      setIsLoggedIn(true);
     } else {
-      props.setIsLoggedIn(true);
-      return;
+      createFavoritesDoc();
+      setIsLoggedIn(true);
     }
   };
 
@@ -44,83 +54,67 @@ const Title = (props) => {
 
   const signOutHandler = () => {
     signOut(auth).then(() => {
-      props.setIsLoggedIn(false);
+      setIsLoggedIn(false);
       setUserName(false);
     });
   };
 
   useEffect(() => {
-    if (props.isLoggedIn) {
-      if (favoritesListExist) {
-        console.log('updateFavorites is running');
-        const updateFavorites = async () => {
-          const newFavs = { favorites: favContext.favoriteItems };
-          const userRef = query(
-            collection(db, 'favorites'),
-            where('id', '==', auth.currentUser.uid)
-          );
-          const findUsers = await getDocs(userRef);
-          findUsers.forEach(async (user) => {
-            const getUser = doc(db, 'favorites', user.id);
-            await updateDoc(getUser, newFavs);
-          });
-        };
-        updateFavorites();
-      } else {
-        console.log('saveFavorites is running');
-        const saveFavorites = async () => {
-          await addDoc(favoritesCollectionRef, {
-            user: auth.currentUser.displayName,
-            id: auth.currentUser.uid,
-            favorites: favContext.favoriteItems,
-          });
-        };
-        saveFavorites();
-        setFavoritesListExist(true);
-      }
+    if (isLoggedIn) {
+      const updateFavorites = async () => {
+        const newFavs = { favorites: favContext.favoriteItems };
+        const userRef = query(
+          collection(db, 'favorites'),
+          where('id', '==', auth.currentUser.uid)
+        );
+        const findUsers = await getDocs(userRef);
+        findUsers.forEach(async (user) => {
+          const getUser = doc(db, 'favorites', user.id);
+          await updateDoc(getUser, newFavs);
+        });
+      };
+      updateFavorites();
     }
-  }, [
-    favContext.favoriteItems,
-    favoritesListExist,
-    favoritesCollectionRef,
-    props.isLoggedIn,
-  ]);
+  }, [favContext.favoriteItems, isLoggedIn]);
 
   const totalFavItems = favContext.favoriteItems.length;
 
   return (
-    <div className={styles.container}>
-      <div className={styles['logo-container']}>
-        <span className={styles['logo']}>Blueprint</span>
-      </div>
+    <React.Fragment>
       {userName && (
         <div className={styles['user-greeting']}>
           <p>Hello, {userName}</p>
         </div>
       )}
-      <div className={styles['account-container']}>
-        {props.isLoggedIn ? (
-          <button className={styles['logout']} onClick={signOutHandler}>
-            Logout
+      <div className={styles.container}>
+        <div className={styles['logo-container']}>
+          <span className={styles['logo']}>Blueprint</span>
+        </div>
+
+        <div className={styles['account-container']}>
+          {isLoggedIn ? (
+            <button className={styles['logout']} onClick={signOutHandler}>
+              Logout
+            </button>
+          ) : (
+            <button className={styles['login']} onClick={signInHandler}>
+              Login
+            </button>
+          )}
+          <button
+            onClick={favContext.favoritesToggleHandler}
+            className={styles['favorites-list']}
+          >
+            <img
+              src={require('../../Assets/images/heart.png')}
+              alt={'Favorites button'}
+              className={styles.image}
+            />
+            <span className={styles['favorites-counter']}>{totalFavItems}</span>
           </button>
-        ) : (
-          <button className={styles['login']} onClick={signInHandler}>
-            Login
-          </button>
-        )}
-        <button
-          onClick={favContext.favoritesToggleHandler}
-          className={styles['favorites-list']}
-        >
-          <img
-            src={require('../../Assets/images/heart.png')}
-            alt={'Favorites button'}
-            className={styles.image}
-          />
-          <span className={styles['favorites-counter']}>{totalFavItems}</span>
-        </button>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
